3a50286b2d59718a0feb4a5219177c5843df1896
agkkkkk <78592748+agkkkkk@users.noreply.github.com>
2021-02-05 18:06:35 +0530

    MapBox

---
+++ script.js
@@ -0,0 +1,33 @@
+// Enter-your-access-token
+mapboxgl.accessToken = "Access token here";
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
+});
+
+function successLocation(position) {
+  console.log(position);
+  setUpMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+  setUpMap([19.075984, 72.877656]);
+}
+
+function setUpMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 15,
+  });
+
+  const nav = new mapboxgl.NavigationControl();
+  map.addControl(nav);
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+  });
+
+  map.addControl(directions, "top-left");
+}
